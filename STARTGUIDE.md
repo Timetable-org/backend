f7725be0e9e52a8afb3c6bf4d61b68416e02c290
Vovchanchyk <60846596+vovchanchyk@users.noreply.github.com>
2024-08-13 23:09:46 +0300

    #4 feat: add-database
    * feature: add postgres database & prisma client
    
    * fix: one to many & make commands clicky
    
    * fix: update STARTGUIDE.md
    
    * feat: make STARTGUIDE.md clicky
    
    * fix: remove mistaken migratin

---
+++ STARTGUIDE.md
@@ -0,0 +1,20 @@
+### 1 run docker services to create or up database 
+```
+yarn services:up
+```
+
+### 2 create migration  
+```
+yarn migration:create
+```
+
+### 3 run migration  
+```
+yarn migration:create
+```
+
+### 4 generate or regenerate prisma client to update db client  
+```
+yarn prisma:generate
+```
+
---
+++ prisma/migrations/20240813195918_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
