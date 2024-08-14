3ae04b783dd9c26ac3d8c7ccc021c33cb2d87932
Vovchanchyk <oberigfast@gmail.com>
2024-08-14 17:17:36 +0300

    feat: add prisma client & user service

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from 'express';
+import { UserService } from '../services';
 
-export const getUsers = (req: Request, res: Response) =>
-  res.send('get all users');
+const service = new UserService();
 
-export const getUser = (req: Request, res: Response) => res.send('get user');
+export const getUsers = async (req: Request, res: Response) => {
+  const users = await service.getUsers();
+  res.status(201);
+  res.send(users);
+};
 
-export const createUser = (req: Request, res: Response) =>
-  res.send('create a users');
+export const getUser = async (req: Request, res: Response) => {
+  const userId = Number(req.params.id);
+  const user = await service.getUser(userId);
+  res.status(201);
+  res.send(user);
+};
+
+export const createUser = async (
+  req: Request<{ name: string; email: string }>,
+  res: Response,
+) => {
+  const { name, email } = req.body;
+  const user = await service.createUser(name, email);
+  res.send(user);
+};
---
+++ src/services/userService.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export class UserService {
+  getUsers() {
+    return prisma.user.findMany();
+  }
+
+  getUser(id: number) {
+    return prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  createUser(name: string, email: string) {
+    return prisma.user.create({
+      data: { name, email },
+    });
+  }
+}
