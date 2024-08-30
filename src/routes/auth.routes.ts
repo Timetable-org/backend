99212c591913c96edf85c1be680409472fd936ce
Vovchanchyk <oberigfast@gmail.com>
2024-08-30 18:39:02 +0300

    feat: add validation

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { logIn, register } from '../controllers/auth.controller';
+import { userSchema } from '../validations-schemas/create-user.schema';
+import { checkSchema } from 'express-validator';
+import { validate } from '../middlewares/validation.middleware';
 
 const router = express.Router();
 
-router.post('/login', logIn);
+router.post('/login', checkSchema(userSchema), validate, logIn);
 
-router.post('/signup', register);
+router.post('/signup', checkSchema(userSchema), validate, register);
 
 export default router;
